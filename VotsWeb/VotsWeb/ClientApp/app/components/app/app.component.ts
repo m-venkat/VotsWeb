3ba9d0e0c2c2f64205b94cce6837b7fb64d6e5bd
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2017-12-06 23:49:36 +1100

    Now full screen toggling works

--- VotsWeb/VotsWeb/ClientApp/app/components/app/app.component.ts
+++ VotsWeb/VotsWeb/ClientApp/app/components/app/app.component.ts
@@ -5,5 +5,57 @@ import { Component, ViewEncapsulation } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
+    ToggleFullScreen(event: any) {
+        this.LaunchFullScreen(event.target);
+    }
+
+    private LaunchFullScreen(element:HTMLElement) {
+        var document: any = window.document;
+         if ((document.fullscreenElement && document.fullscreenElement !== null) ||
+            (!document.mozFullScreen  && !document.webkitIsFullScreen)) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullScreen) {
+                document.documentElement.webkitRequestFullScreen();
+             }
+
+            //element.style.border = "1px solid #dcdcdc";
+            //element.style.color = "#dcdcdc";
+            
+
+        } else {
+             if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        }  
+
+        //if ((document.fullscreenElement && document.fullscreenElement !== null) ||
+        //    (!document.mozFullScreen  && !document.webkitIsFullScreen)) {
+        //    if (document.documentElement.requestFullscreen) {
+        //        document.documentElement.requestFullscreen();
+        //    } else if (document.documentElement.mozRequestFullScreen) {
+        //        document.documentElement.mozRequestFullScreen();
+        //    } else if (document.documentElement.webkitRequestFullScreen) {
+        //        document.documentElement.webkitRequestFullScreen();
+        //    }
+        //} else {
+        //    if (document.cancelFullScreen) {
+        //        document.cancelFullScreen();
+        //    } else if (document.mozCancelFullScreen) {
+        //        document.mozCancelFullScreen();
+        //    } else if (document.webkitCancelFullScreen) {
+        //        document.webkitCancelFullScreen();
+        //    }
+        //}  
+    }
 }
