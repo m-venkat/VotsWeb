7676174fdcad61e22df58a7114c7b7b4a72d1726
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2018-05-19 20:27:19 +1000

    Now container and header section and footer section working fine

--- vots-web-app/ClientApp/src/app/app.component.ts
+++ vots-web-app/ClientApp/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ToggleFullScreen(event: any) {
+    this.LaunchFullScreen(event.target);
+  }
+
+  private LaunchFullScreen(element: HTMLElement) {
+    var document: any = window.document;
+    if ((document.fullscreenElement && document.fullscreenElement !== null) ||
+      (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullScreen) {
+        document.documentElement.webkitRequestFullScreen();
+      }
+    } else {
+      if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+      }
+    }
+  }
 }
