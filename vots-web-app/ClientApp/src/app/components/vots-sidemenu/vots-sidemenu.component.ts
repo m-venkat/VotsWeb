1b2c73bd5a3b5e4c2d8fbed5391ba8093dc6de3c
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2018-08-27 06:57:13 +1000

    Trying AutoComplete with Fully with Observables

--- vots-web-app/ClientApp/src/app/components/vots-sidemenu/vots-sidemenu.component.ts
+++ vots-web-app/ClientApp/src/app/components/vots-sidemenu/vots-sidemenu.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data-service/data.service';
 import { VotsMenu, VotsMenuItem, MenuItems, VotsMenuParent } from '../../models/MenuModels';
 import { Observable } from 'rxjs';
-import { List } from 'linqts'
-import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from '@angular/material';
+import { List } from 'linqts';
+import { MatInputModule } from '@angular/material/input';
+import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader,MatInput } from '@angular/material';
+import { from, of} from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-vots-sidemenu',
@@ -26,9 +29,42 @@ export class VotsSidemenuComponent implements OnInit {
     alert("Selected.."+JSON.stringify(selectedMenu));
   }
 
+  GetVotsSideMenu(): Observable<VotsMenuParent[]> {
+    return this.dataService.GetVotsMenu()
+      .pipe
+      (
+      map
+        (
+        item => {
+          let parentMenus = item.map(item => ({ parentMenuId: item.ParentMenuId, parentMenuLabel: item.ParentMenuLabel }))
+            .filter((thing, index, self) =>
+              index === self.findIndex((t) => (
+                t.parentMenuId === thing.parentMenuId && t.parentMenuLabel === thing.parentMenuLabel
+              ))
+            );
+          parentMenus.forEach(mnu => {
+            let parentWithChild = item.filter(item => item.ParentMenuId == mnu.parentMenuId)
+              .filter((thing, index, self) =>
+                index === self.findIndex((t) => (
+                  t.ChildMenuId === thing.ChildMenuId && t.ParentMenuId === thing.ParentMenuId
+                ))
+              )
+            let pm = <VotsMenuParent>{};
+            pm.ParentMenuId = mnu.parentMenuId;
+            pm.ParentMenuLabel = mnu.parentMenuLabel;
+            pm.MenuItems = parentWithChild;
+            this.parentMenu.push(pm);
+            
+          }//end of item processing
+            return this.parentMenu;
+        }
+           
+      )// End of map
+        
+  }
+  
+  /*
   GetVotsSideMenu(): void {
-    ///this.menu = this.dataService.GetVotsMenu();
-    
     this.dataService.GetVotsMenu().subscribe(data => {
       let menu: VotsMenuItem[];
       menu = data;
@@ -58,5 +94,5 @@ export class VotsSidemenuComponent implements OnInit {
        },
        error => { }
      );
-  }
+  }*/
 }
